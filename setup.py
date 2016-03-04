e779dc56eba538b95818d90bd9a3cee740adef42
Shawn Averkamp <shawnaverkamp@gmail.com>
2016-03-04 10:07:30 -0500

    package modsqual

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='modsqual',
     version='0.5.0',
+    py_modules=['modsqual'],
     description='An lxml wrapper for working with MODS XML data.',
     url='https://github.com/saverkamp/modsqual',
     author='Shawn Averkamp',
